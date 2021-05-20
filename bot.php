a214a80f87bf4cc468b9d139b7f49ef3ef3478dd
Peter <47149688+mccolaichuk@users.noreply.github.com>
2021-05-20 18:01:51 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,15 @@
-// API here: https://dogechain.info/api/websocket
-const $ws_host = "wss://ws.dogechain.info/inv";
-const $ws_subscr_to_address = "{"op":"addr_sub", "addr":"dogecoin_address"}";
+<?php
+require "./websocket_client.php";
+$server = 'wss://ws.dogechain.info/inv';
+$message = "{"op":"addr_sub", "addr":"$dogecoin_address"}";
 
+if( $sp = websocket_open($server, 443,'',$errstr, 10,true) ) {
+  echo "Sending message to server: '$message' \n";
+  websocket_write($sp,$message);
+  echo "Server responed with: '" . websocket_read($sp,$errstr) ."'\n";
+}else {
+  echo "Failed to connect to server\n";
+  echo "Server responed with: $errstr\n";
+}
+
+?>
